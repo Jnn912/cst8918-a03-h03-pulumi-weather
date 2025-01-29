86691bfaaca186b2705fdaa37119328d354ce3bc
[Akhil Jose] <[akhiljos256@gmail.com]>
2025-01-28 23:10:12 -0500

    hybrid

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,1 +1,39 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as cache from '@pulumi/azure-native/cache'
+// ... configs and resource group
+
+
+
+// Create a managed Redis service
+const redis = new cache.Redis(`${prefixName}-redis`, {
+  name: `${prefixName}-weather-cache`,
+  location: 'westus3',
+  resourceGroupName: resourceGroup.name,
+  enableNonSslPort: true,
+  redisVersion: 'Latest',
+  minimumTlsVersion: '1.2',
+  redisConfiguration: {
+    maxmemoryPolicy: 'allkeys-lru',
+  },
+  sku: {
+    name: 'Basic',
+    family: 'C',
+    capacity: 0,
+  },
+})
+
+
+// Extract the auth creds from the deployed Redis service
+const redisAccessKey = cache
+  .listRedisKeysOutput({
+    name: redis.name,
+    resourceGroupName: resourceGroup.name,
+  })
+  .apply((keys) => keys.primaryKey)
+const redisConnectionString = pulumi.interpolate`rediss://:${redisAccessKey}@${redis.hostName}:${redis.sslPort}`
+environmentVariables: [
+    // existing vars ...
+    {
+      name: 'REDIS_URL',
+      value: redisConnectionString,
+    },
+  ]
